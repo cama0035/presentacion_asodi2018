a1b17f314ce0a8182bb9c6dbb598208b3da0766f
proyecto-dashboard-cm1 <cama0035@gmail.com>
2023-03-14 16:03:23 -0300

    Update

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import streamlit as st
-import plotly.express as px
+import plotly-express as px
 from PIL import Image
 
 st.set_page_config(page_title='HEMODIÁLISIS CRÓNICA EN CHILE')
